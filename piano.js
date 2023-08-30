5181ef414cbefc5105fdf4cc32638513e2264bbc
AyaALAMA <121454295+AyaALAMA@users.noreply.github.com>
2023-08-30 22:48:35 +0300

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,35 @@
+function play(num)
+{
+    var audio = new Audio("sound/note" + num + ".wav");
+    audio.play();
+    // if (x=='do'){
+        // var audio = new Audio("sound/note1")
+        // 
+    // }
+    //  else if (x=='re'){
+       // alert("$")
+        // 
+        // var audio=new Audio("sound/note1")
+    // }
+    // else if (x=='me'){
+      // alert("^")
+    //    var audio=new Audio("sound/note2")
+    // }
+    // else if (x=='fa'){
+      // alert("%")
+        // var audio=new Audio("sound/note3")
+    // }
+    // else if (x=='so'){
+       // alert("*")
+        // var audio=new Audio("sound/note4")
+    // }
+    // else if (x=='la'){
+       // alert("!")
+        // var audio=new Audio("sound/note6")
+    // }
+    // else if (x=='se'){
+//alert("-")
+    //    var audio=new Audio("sound/note5")
+    // }
+
+ }
